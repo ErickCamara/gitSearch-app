aa1032fc5101d570a4896a8975b2189993870d0a
Erick Santos <erick.santos@digivox.com.br>
2020-03-15 20:17:09 -0300

    teste passando o codepush key no arquivo strings.xml

--- android/app/src/main/java/com/gitinfo/MainApplication.java
+++ android/app/src/main/java/com/gitinfo/MainApplication.java
@@ -25,9 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(
-              new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG)
-            );
+        //   packages.add(
+        //       new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG)
+        //     );
           return packages;
         }
 
