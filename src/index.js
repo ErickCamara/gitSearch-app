b729eb388127d756d1e74d6ddb53e468e8d71f5a
Erick Santos <erick.santos@digivox.com.br>
2020-03-15 18:50:49 -0300

    Removendo configuração de chave do code-push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
+// import CodePush from 'react-native-code-push'
+// import OneSignal from 'react-native-onesignal'
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
 export default class App extends Component {
-  render() {
-    return (
-        <>
-            <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
-            <Routes/>
-        </>
-    );
-  }
-};
+    // constructor(props) {
+    //     OneSignal.init('1f0f7fbe-93af-421b-9feb-2b1c390399c7');
+    //     OneSignal.addEventListener('received', this.onReceived);
+    //     OneSignal.addEventListener('opened', this.onOpened);
+    //     OneSignal.addEventListener('ids', this.onIds);
+    // }
+
+    // componentWillUnmount() {
+    //     OneSignal.removeEventListener('received', this.onReceived);
+    //     OneSignal.removeEventListener('opened', this.onOpened);
+    //     OneSignal.removeEventListener('ids', this.onIds);
+    // }
+
+    // onReceived = (data) => {
+
+    // }
+    // onOpened = (notification) => {
+
+    // }
+    // onIds = (id) => {
+
+    // }
+
+    render() {
+        return (
+            <>
+                <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
+                <Routes/>
+            </>
+        );
+    }
+    };
+
+// export default CodePush({
+//     checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+// })(App);
