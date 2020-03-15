81d584e1d4811d052ce0e5f39fee63099d5cd200
Erick Santos <erick.santos@digivox.com.br>
2020-03-15 20:28:37 -0300

    alter codepush configuration

--- android/app/src/main/java/com/gitinfo/MainApplication.java
+++ android/app/src/main/java/com/gitinfo/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -36,10 +37,10 @@ public class MainApplication extends Application implements ReactApplication {
           return "index";
         }
 
-        // @Override
-        // protected String getJSBundleFile() {
-        //     return CodePush.getJSBundleFile();
-        // }
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
       };
 
   @Override
